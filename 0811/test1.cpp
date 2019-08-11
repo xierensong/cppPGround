8f0dcc993f0cc905f0bd59cb627ddbf90d09a0fc
xierensong <283318608@qq.com>
2019-08-11 09:09:59 +0800

    first example

---
+++ 0811/test1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello, my vs code!" << endl;
+    vector<string> strings{"hello", " ", "world", "!"};
+    for (auto str:strings) {
+        cout << str << ' ';
+    }
+    cout << endl;
+    return 0;
+}
