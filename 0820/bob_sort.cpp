0c60d3d42f718fa1e5ae875f32616883a0648282
xiexie1357 <283318608@qq.com>
2019-09-07 09:50:17 +0800

    sort

--- 0820/bob_sort.cpp
+++ 0820/bob_sort.cpp
@@ -5,12 +5,24 @@ using namespace std;
 void bob_sort(vector<int> &nums) 
 {
     int len = nums.size();
-    
+    for(int i = len-1; i >= 0; --i) {
+        for(int j = 0; j < i; ++j) {
+            if (nums[j] > nums[j+1]) {
+                int tmp = nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=tmp;
+            }
+        }
+    }
 }
 
 int main()
 {
     vector<int> nums{10,2,5,9,100,27,6};
     bob_sort(nums);
+    for(auto num:nums) {
+        cout <<num << ' ';
+    }
+    cout << endl;
     return 0;
 }
--- 0820/merge_sort_e.cpp
+++ 0820/merge_sort_e.cpp
@@ -2,24 +2,13 @@
 
 using namespace std;
 
-void print(vector<int> nums) {
-    for(auto num :nums) {
-        cout << num << ' ';
-    }
-    cout << endl;
-}
-
 void merge(vector<int> nums1, vector<int> &nums2, int low, int mid, int high)
 {
-    cout << "merge " << low << ' ' << high << endl;
-    if(low == high) {
-        return ;
-    }
     int i = low, j = mid+1;
     int k = low;
     while(i <= mid && j <= high) {
-        if (nums1[i] < nums1[j]) {
-            nums2[k]= nums1[i];
+        if (nums1[i] <nums1[j]) {
+            nums2[k]=nums1[i];
             i++;
         }
         else {
@@ -33,35 +22,40 @@ void merge(vector<int> nums1, vector<int> &nums2, int low, int mid, int high)
         i++;
         k++;
     }
-    while (j <= high) {
+    while(j <= high) {
         nums2[k]=nums1[j];
         j++;
         k++;
-    }    
+    }
 }
 
-//nums归并到nums2
-void merge_sort(vector<int> nums, vector<int> &nums2, int low, int high)
+void merge_sort(vector<int> nums1, vector<int> &nums2, int low, int high)
 {
-    if(low == high) {
-        nums2=nums;
+    cout << "wer" << endl;
+    if (low == high) {
+        nums2=nums1;
     }
     else {
         int mid = (low+high)/2;
-        vector<int> nums1=nums;
-        merge_sort(nums,nums1, low, mid);
-        merge_sort(nums,nums1, mid+1, high);
-        cout << "m" << endl;
-        print(nums1);
-        merge(nums1, nums2, low, mid, high);
-        cout << "mm" << endl;
-        print(nums2);
+        vector<int> nums3=nums1;
+        merge_sort(nums1, nums3, low, mid);
+        merge_sort(nums1, nums3, mid+1, high);
+        for(auto num : nums3) {
+            cout << num << " ";
+        }
+        cout << endl;
+        merge(nums3,nums2,low,mid,high);
     }
 }
 
 int main()
 {
-    vector<int> nums{1,8,10,5,2,300,20};
-    merge_sort(nums,nums, 0, nums.size()-1);
-    print(nums);
+    cout << "werq" << endl;
+    vector<int> nums{10,2,5,9,100,27,6};
+    merge_sort(nums,nums,0,nums.size()-1);
+    cout << "werw" << endl;
+    for(auto num : nums) {
+        cout << num << ' ';
+    }
+    cout << endl;
 }
