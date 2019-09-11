d16cec9aa16c580ed43d3a65d09534c5bbf9da69
xierensong <283318608@qq.com>
2019-09-11 22:19:51 +0800

    adf

--- list/1.cpp
+++ list/1.cpp
@@ -8,21 +8,22 @@ struct Node {
     Node(int my_val):val(my_val),next(nullptr) {};
 };
 
-void createList(Node* list)
+void createList(Node* &list)
 {
     int my_val;
     string line;
     getline(cin,line);
     stringstream ss(line);
+    list=new Node(0);
     Node* p=list;
     while(ss >> my_val) {
-        cout << "1" << endl;
+        //cout << "1 " << my_val << endl;
         Node* curr_node =new Node(my_val);
-        list->next=curr_node;
-        list=list->next;
+        p->next=curr_node;
+        p=p->next;
     }
-    list=p->next;
-    cout << "end create" << endl;
+    list=list->next;
+    //cout << "end create" << endl;
 }
 
 void printList(Node* list)
@@ -30,9 +31,23 @@ void printList(Node* list)
     Node *p=list;
     while(p)
     {
-        cout << p->val << endl;
+        cout << p->val << ' ';
         p=p->next;
     }
+    cout << endl;
+}
+
+void reverse(Node* &list)
+{
+    Node* pre=nullptr, *curr=list, *next=nullptr;
+    while(curr)
+    {
+        next=curr->next;
+        curr->next=pre;
+        pre=curr;
+        curr=next;
+    }
+    list=pre;
 }
 
 int main()
@@ -40,5 +55,8 @@ int main()
     Node * my_list;
     createList(my_list);
     cout << "print list" << endl;
+    printList(my_list);
+    cout << "reverse list" << endl;
+    reverse(my_list);
     printList(my_list);
 }
